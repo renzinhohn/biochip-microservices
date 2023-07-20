d4bb21634be7f6b093b85bfe66a5cde411b0a332
renzinhohn <renzo.hn@gmail.com>
2023-07-20 12:49:21 -0500

    gatewayserver: Adding Spring Cloud Gateway configuration

---
+++ services/api-services/biochip-gateway-service/src/main/java/com/biochip/gateway/BiochipGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.biochip.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiochipGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiochipGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ services/api-services/biochip-gateway-service/src/test/java/com/biochip/gateway/BiochipGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biochip.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiochipGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
