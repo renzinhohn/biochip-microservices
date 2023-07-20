74e49d0bbeff4f0d4c4418ca9e1d7a4dbb999d14
renzinhohn <renzo.hn@gmail.com>
2023-07-20 16:59:27 -0500

    circuitbreaker: Adding Resilience4j configuration

--- services/core-services/biochip-customer-service/src/main/java/com/biochip/costumer/controller/CustomerController.java
+++ services/core-services/biochip-customer-service/src/main/java/com/biochip/costumer/controller/CustomerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,8 @@ import com.biochip.costumer.entity.Customer;
 import com.biochip.costumer.models.Automobile;
 import com.biochip.costumer.models.Motorcycle;
 import com.biochip.costumer.service.CustomerService;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 @RequestMapping("/customer")
@@ -49,6 +52,7 @@ public class CustomerController {
 		return ResponseEntity.ok(newCustomer);
 	}
 	
+	@CircuitBreaker(name = "automobilesCB", fallbackMethod = "fallBackGetAutomobiles")
 	@GetMapping("/automobiles/{customerId}")
 	public ResponseEntity<List<Automobile>> getAutomobiles(@PathVariable("customerId") Long customerId){
 		Customer customer = customerService.getCustomerById(customerId);
@@ -59,6 +63,7 @@ public class CustomerController {
 		return ResponseEntity.ok(cars);
 	}
 
+	@CircuitBreaker(name = "motorcyclesCB", fallbackMethod = "fallBackGetMotorcycles")
 	@GetMapping("/motorcycles/{customerId}")
 	public ResponseEntity<List<Motorcycle>> getMotorcycles(@PathVariable("customerId") Long customerId){
 		Customer customer = customerService.getCustomerById(customerId);
@@ -69,6 +74,7 @@ public class CustomerController {
 		return ResponseEntity.ok(motorcycles);
 	}
 	
+	@CircuitBreaker(name = "automobilesCB", fallbackMethod = "fallBackSaveAutomobile")
 	@PostMapping("/automobile/{customerId}")
 	public ResponseEntity<Automobile> saveAutomobile(@PathVariable("customerId") Long customerId, @RequestBody Automobile automobile){
 		Customer customer = customerService.getCustomerById(customerId);
@@ -79,6 +85,7 @@ public class CustomerController {
 		return ResponseEntity.ok(newAutomobile);
 	}
 	
+	@CircuitBreaker(name = "motorcyclesCB", fallbackMethod = "fallBackSaveMotorcycle")
 	@PostMapping("/motorcycle/{customerId}")
 	public ResponseEntity<Motorcycle> saveMotorcycle(@PathVariable("customerId") Long customerId, @RequestBody Motorcycle motorcycle){
 		Customer customer = customerService.getCustomerById(customerId);
@@ -89,9 +96,30 @@ public class CustomerController {
 		return ResponseEntity.ok(newMotorcycle);
 	}
 	
+	@CircuitBreaker(name = "vehiclesCB", fallbackMethod = "fallBackGetAllVehicles")
 	@GetMapping("/vehicles/{customerId}")
 	public ResponseEntity<Map<String, Object>> getAllVehicles(@PathVariable("customerId") Long customerId){
 		Map<String, Object> result = customerService.getAllVehicles(customerId);
 		return ResponseEntity.ok(result);
+	}
+	
+	private ResponseEntity<List<Automobile>> fallBackGetAutomobiles(@PathVariable("customerId") Long customerId, RuntimeException e){
+		return new ResponseEntity("The customer: "+customerId+" have their automobiles in the workshop", HttpStatus.OK);
+	}
+	
+	private ResponseEntity<List<Automobile>> fallBackSaveAutomobile(@PathVariable("customerId") Long customerId, @RequestBody Automobile automobile, RuntimeException e){
+		return new ResponseEntity("The customer: "+customerId+" don't have enough money to pay the automobile", HttpStatus.OK);
+	}
+	
+	private ResponseEntity<List<Motorcycle>> fallBackGetMotorcycles(@PathVariable("customerId") Long customerId, RuntimeException e){
+		return new ResponseEntity("The customer: "+customerId+" have their motorcycles in the workshop", HttpStatus.OK);
+	}
+	
+	private ResponseEntity<List<Motorcycle>> fallBackSaveMotorcycle(@PathVariable("customerId") Long customerId, @RequestBody Motorcycle motorcycle, RuntimeException e){
+		return new ResponseEntity("The customer: "+customerId+" don't have enough money to pay the motorcycle", HttpStatus.OK);
+	}
+	
+	private ResponseEntity<Map<String, Object>> fallBackGetAllVehicles(@PathVariable("customerId") Long customerId, RuntimeException e){
+		return new ResponseEntity("The customer: "+customerId+" have all their vehicles in the workshop", HttpStatus.OK);
 	}
 }
