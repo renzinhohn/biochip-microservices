d98ac7fe0c81fa54931662569be231b53ac5d834
renzinhohn <renzo.hn@gmail.com>
2023-07-20 00:32:18 -0500

    eurekaserver: Adding Spring Cloud Discovery Eureka configuration

---
+++ services/configuration-services/biochip-discovery-service/src/test/java/com/biochip/discovery/BiochipDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biochip.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiochipDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- services/core-services/biochip-automobile-service/src/main/java/com/biochip/automobile/BiochipAutomobileServiceApplication.java
+++ services/core-services/biochip-automobile-service/src/main/java/com/biochip/automobile/BiochipAutomobileServiceApplication.java
@@ -2,7 +2,9 @@ package com.biochip.automobile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class BiochipAutomobileServiceApplication {
 
