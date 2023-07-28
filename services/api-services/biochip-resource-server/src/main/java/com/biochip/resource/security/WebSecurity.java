7c7b92bed270f55f5b45028fef427d95476a2ee9
renzinhohn <renzo.hn@gmail.com>
2023-07-27 23:54:19 -0500

    resourceserver_with_roles: Adding configuration to use roles

--- services/api-services/biochip-resource-server/src/main/java/com/biochip/resource/security/WebSecurity.java
+++ services/api-services/biochip-resource-server/src/main/java/com/biochip/resource/security/WebSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,14 +15,21 @@ public class WebSecurity {
 	@Bean
 	SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		// Configure Http Security
-		// Scope based Access Control
+		// Role based Access Control
+		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+		
 		http.authorizeHttpRequests()
 				.antMatchers(HttpMethod.GET, "/users/status/check")
-				.hasAuthority("SCOPE_profile")
+				//.hasAuthority("SCOPE_profile")
+				//.hasRole("developer")
+				.hasAuthority("ROLE_developer")
+				//.hasAnyRole("developer", "user")
 			.anyRequest().authenticated()
 			.and()
 			.oauth2ResourceServer()
-			.jwt();
+			.jwt()
+			.jwtAuthenticationConverter(jwtAuthenticationConverter);
 
 		return http.build();
 	}
